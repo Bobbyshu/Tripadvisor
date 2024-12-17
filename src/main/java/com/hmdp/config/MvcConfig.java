fdf1417ef42718299683d8d288449034ec3f5712
Bobbyshu <shu.yun@northeastern.edu>
2024-12-16 23:41:17 -0500

    fix(): fix bug exclude service

--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -10,7 +10,7 @@ public class MvcConfig implements WebMvcConfigurer {
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(new LoginInterceptor()).excludePathPatterns(
-        "/user/code", "user/login", "blog/hot", "shop/**", "shop-type/**", "upload/**", "voucher/**"
+        "/user/code", "/user/login", "/blog/hot", "/shop/**", "/shop-type/**", "/upload/**", "/voucher/**"
     );
   }
 }
