a6195bd30f850f42a51933163ea6ca448e8eb8b7
Bobbyshu <shu.yun@northeastern.edu>
2024-12-16 23:01:30 -0500

    feat(): encapsulate user info into UserDTO

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,6 +1,6 @@
 package com.hmdp.utils;
 
-import com.hmdp.entity.User;
+import com.hmdp.dto.UserDTO;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,7 +18,7 @@ public class LoginInterceptor implements HandlerInterceptor {
     }
 
     // store user info into threadlocal
-    UserHolder.saveUser((User) user);
+    UserHolder.saveUser((UserDTO) user);
     return true;
   }
 
