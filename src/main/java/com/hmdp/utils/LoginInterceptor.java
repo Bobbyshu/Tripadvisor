2380161654aec75a54c44c348619012d3707ecf4
Bobbyshu <shu.yun@northeastern.edu>
2024-12-17 01:00:06 -0500

    feat(): update login and send code service with redis

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,24 +1,48 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.hmdp.dto.UserDTO;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class LoginInterceptor implements HandlerInterceptor {
+  private StringRedisTemplate stringRedisTemplate;
+
+  // must use constructor because this class isn't being managed by spring bean
+  public LoginInterceptor(StringRedisTemplate stringRedisTemplate) {
+    this.stringRedisTemplate = stringRedisTemplate;
+  }
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-    HttpSession session = request.getSession();
-    Object user = session.getAttribute("user");
-    if (user == null) {
+    String token = request.getHeader("authorization");
+    if (StrUtil.isBlank(token)) {
       response.setStatus(401);
       return false;
     }
 
-    // store user info into threadlocal
-    UserHolder.saveUser((UserDTO) user);
+    // got user by token
+    String key = RedisConstants.LOGIN_USER_KEY + token;
+    Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(key);
+    if (userMap.isEmpty()) {
+      response.setStatus(401);
+      return false;
+    }
+
+    // transfer hash data to DTO object
+    UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+
+    // store user info into Threadlocal
+    UserHolder.saveUser(userDTO);
+
+    // update token valid time
+    stringRedisTemplate.expire(key, RedisConstants.LOGIN_USER_TTL, TimeUnit.MINUTES);
     return true;
   }
 
