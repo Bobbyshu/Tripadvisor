3304f364a1a1904117c345b26773dc20d9d3a105
Bobbyshu <shu.yun@northeastern.edu>
2024-12-18 12:13:36 -0500

    refactor(): encapsulate redis set methods

---
+++ src/main/java/com/hmdp/utils/CacheClient.java
@@ -0,0 +1,37 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.entity.Shop;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+import static com.hmdp.utils.RedisConstants.*;
+
+@Slf4j
+@Component
+public class CacheClient {
+
+  private final StringRedisTemplate stringRedisTemplate;
+  public CacheClient(StringRedisTemplate stringRedisTemplate) {
+    this.stringRedisTemplate = stringRedisTemplate;
+  }
+
+  public void set(String key, Object value, Long time, TimeUnit unit) {
+    stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(value), time, unit);
+  }
+
+  public void setWithLogicalExpire(String key, Object value, Long time, TimeUnit unit) {
+    RedisData redisData = new RedisData();
+    redisData.setData(value);
+    redisData.setExpireTime(LocalDateTime.now().plusSeconds(unit.toSeconds(time)));
+    stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(redisData));
+  }
+
+
+}
