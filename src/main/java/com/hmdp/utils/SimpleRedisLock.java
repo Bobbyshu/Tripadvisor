ab2fd898241563512618e08da91e083876ee2b3c
Bobbyshu <shu.yun@northeastern.edu>
2024-12-19 16:36:26 -0500

    feat(): unlock replace with lua script for Atomic Operation

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,9 +1,13 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 import org.yaml.snakeyaml.events.Event;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
@@ -18,6 +22,12 @@ public class SimpleRedisLock implements ILock {
 
   private static final String KEY_PREFIX = "lock:";
   private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+  private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+  static {
+    UNLOCK_SCRIPT = new DefaultRedisScript<>();
+    UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+    UNLOCK_SCRIPT.setResultType(Long.class);
+  }
 
   @Override
   public boolean tryLock(long timeoutSec) {
@@ -32,10 +42,17 @@ public class SimpleRedisLock implements ILock {
 
   @Override
   public void unlock() {
-    String threadID = ID_PREFIX + Thread.currentThread().getId();
-    String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-    if (threadID.equals(id)) {
-      stringRedisTemplate.delete(KEY_PREFIX + name);
-    }
+    stringRedisTemplate.execute(
+        UNLOCK_SCRIPT,
+        Collections.singletonList(KEY_PREFIX + name),
+        ID_PREFIX + Thread.currentThread().getId());
   }
+//  @Override
+//  public void unlock() {
+//    String threadID = ID_PREFIX + Thread.currentThread().getId();
+//    String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//    if (threadID.equals(id)) {
+//      stringRedisTemplate.delete(KEY_PREFIX + name);
+//    }
+//  }
 }
