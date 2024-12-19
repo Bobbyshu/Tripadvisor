c613c5935262e7010165b5688d1e4274be0f0e3a
Bobbyshu <shu.yun@northeastern.edu>
2024-12-19 13:59:24 -0500

    feat(): impl try lock methods

---
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -0,0 +1,33 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+public class SimpleRedisLock implements ILock{
+
+  private String name;
+  private StringRedisTemplate stringRedisTemplate;
+
+  public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
+    this.name = name;
+    this.stringRedisTemplate = stringRedisTemplate;
+  }
+
+  private static final String KEY_PREFIX = "lock:";
+  @Override
+  public boolean tryLock(long timeoutSec) {
+    long threadID = Thread.currentThread().getId();
+    Boolean success = stringRedisTemplate.opsForValue()
+        .setIfAbsent(KEY_PREFIX + name, threadID + "", timeoutSec, TimeUnit.SECONDS);
+
+    // avoid auto unboxing appear null pointer
+    // return success
+    return Boolean.TRUE.equals(success);
+  }
+
+  @Override
+  public void unlock() {
+
+  }
+}
