e7026f70f1b91491be0f5554e46b746f35cc226e
Bobbyshu <shu.yun@northeastern.edu>
2024-12-20 16:36:16 -0500

    feat(): call scripts and check purchase availability(message queue pending)

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -13,12 +13,15 @@ import com.hmdp.utils.UserHolder;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.aop.framework.AopContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
+import java.util.Collections;
 
 /**
  * <p>
@@ -41,40 +44,64 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
   @Resource
   private RedissonClient redissonClient;
 
+  private static final DefaultRedisScript<Long> SECKILL_SCRIPT;
+  static {
+    SECKILL_SCRIPT = new DefaultRedisScript<>();
+    SECKILL_SCRIPT.setLocation(new ClassPathResource("seckill.lua"));
+    SECKILL_SCRIPT.setResultType(Long.class);
+  }
+
   @Override
   public Result seckillVoucher(Long voucherId) {
-    SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
-    // flash sale isn't start
-    if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
-      return Result.fail("flash sale isn't start");
-    }
-
-    // flash sale end
-    if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
-      return Result.fail("flash sale ended");
-    }
-
-    if (voucher.getStock() < 1) {
-      return Result.fail("stock sold out!");
-    }
-
     Long userId = UserHolder.getUser().getId();
-//    SimpleRedisLock lock = new SimpleRedisLock("order" + userId, stringRedisTemplate);
-    RLock lock = redissonClient.getLock("lock:order" + userId);
-
-    boolean isLock = lock.tryLock();
-
-    if (!isLock) {
-      return Result.fail("Each user can only order once!");
+    Long result = stringRedisTemplate.execute(
+        SECKILL_SCRIPT,
+        Collections.emptyList(),
+        voucherId.toString(), userId.toString()
+    );
+    int res = result.intValue();
+    if (res != 0) {
+      return Result.fail(res == 1 ? "no enough stock" : "can't order twice");
     }
 
-    try {
-      IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
-      return proxy.createVoucherOrder(voucherId);
-    } finally {
-      lock.unlock();
-    }
+    long orderId = redisIdWorker.nextId("order");
+    return Result.ok(orderId);
   }
+
+//  @Override
+//  public Result seckillVoucher(Long voucherId) {
+//    SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+//    // flash sale isn't start
+//    if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+//      return Result.fail("flash sale isn't start");
+//    }
+//
+//    // flash sale end
+//    if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+//      return Result.fail("flash sale ended");
+//    }
+//
+//    if (voucher.getStock() < 1) {
+//      return Result.fail("stock sold out!");
+//    }
+//
+//    Long userId = UserHolder.getUser().getId();
+////    SimpleRedisLock lock = new SimpleRedisLock("order" + userId, stringRedisTemplate);
+//    RLock lock = redissonClient.getLock("lock:order" + userId);
+//
+//    boolean isLock = lock.tryLock();
+//
+//    if (!isLock) {
+//      return Result.fail("Each user can only order once!");
+//    }
+//
+//    try {
+//      IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+//      return proxy.createVoucherOrder(voucherId);
+//    } finally {
+//      lock.unlock();
+//    }
+//  }
 
   @Transactional
   public Result createVoucherOrder(Long voucherId) {
