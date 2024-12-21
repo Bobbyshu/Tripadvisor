69dbf82989a88eb3646b8e92884f102b8b4625d9
Bobbyshu <shu.yun@northeastern.edu>
2024-12-21 12:14:41 -0500

    fix(): change lua script by using stream

--- src/main/resources/seckill.lua
+++ src/main/resources/seckill.lua
@@ -1,5 +1,6 @@
 local voucherId = ARGV[1]
 local userId = ARGV[2]
+local orderId = ARGV[3]
 local stockKey = 'seckill:stock:' .. voucherId
 local orderKey = 'seckill:order:' .. voucherId
 
@@ -15,5 +16,6 @@ end
 
 redis.call('incrby', stockKey, -1)
 redis.call('sadd', orderKey, userId)
-
+-- send message to queue
+redis.call('xadd', 'stream.orders', '*', 'userId', userId, 'voucherId', voucherId, 'id', orderId)
 return 0
