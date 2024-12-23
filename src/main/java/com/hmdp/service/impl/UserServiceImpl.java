07f437d34b7dcfecd2fccc4c2b87ccf7eedc595d
Bobbyshu <shu.yun@northeastern.edu>
2024-12-22 20:11:01 -0500

    feat(): impl check-in function

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -12,6 +12,7 @@ import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,8 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -86,6 +89,19 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     stringRedisTemplate.opsForHash().putAll(tokenKey, userMap);
     stringRedisTemplate.expire(tokenKey, LOGIN_USER_TTL, TimeUnit.MINUTES);
     return Result.ok(token);
+  }
+
+  @Override
+  public Result sign() {
+    Long userId = UserHolder.getUser().getId();
+    LocalDateTime now = LocalDateTime.now();
+
+    String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+    String key = USER_SIGN_KEY + userId + keySuffix;
+
+    int dayOfMonth = now.getDayOfMonth();
+    stringRedisTemplate.opsForValue().setBit(key, dayOfMonth - 1, true);
+    return Result.ok();
   }
 
   private User createUserWithPhone(String phone) {
